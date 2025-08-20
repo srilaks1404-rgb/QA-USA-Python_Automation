a6f308e936001128591a69b5b720365ad0e8ca57
srilaks1404-rgb <srilaks1404@gmail.com>
2025-08-20 01:22:13 -0500

    Tasks 2–5: data constants, test skeletons, server check, ice-cream loop

--- data.py
+++ data.py
@@ -1,1 +1,11 @@
+# Test data constants
+
+URBAN_ROUTES_URL = 'https://cnt-d9db26fa-bc4c-40f4-896b-c5d5e89b9a77.containerhub.tripleten-services.com'  # URL from the start server button will go here later
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
 # File with constants
